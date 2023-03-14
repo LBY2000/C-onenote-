c576aa64a60ded3532326c20309cd83eedb9cb87
lby2000 <2504054393@qq.com>
2023-03-14 18:38:35 +0800

    111

--- 目前成功的单线程可扩展hash/ex_hash.cpp
+++ 目前成功的单线程可扩展hash/ex_hash.cpp
@@ -22,6 +22,7 @@ struct KV{
 struct ex_hash *myhash;
 struct segment **temp;
 struct segment **newtemp;
+struct segment *single_segment;
 bool insert(struct ex_hash *myhash,string key,int value){
     std::hash<string> trans;
     size_t hash_value =trans(key);
@@ -73,28 +74,7 @@ void expand(ex_hash *myhash,string key){
     //struct segment *temp[newsize];
     temp=new struct segment*[newsize];
     int loc2=loc1<<1,loc3=loc2+1;
-    for(i=0;i<newsize;i++){/*
-        if(i==loc2){
-            temp[i]=new struct segment;
-            temp[i]->local_depth=myhash->directory[loc1]->local_depth+1;
-            for(j=0;j<1024;j++){
-                for(k=0;k<8;k++){
-                    temp[i]->b[j].kv[k].key="NULL";
-                }
-            }
-                
-            }else if(i==loc3){
-                temp[i]=new struct segment;
-                temp[i]->local_depth=myhash->directory[loc1]->local_depth+1;
-                for(j=0;j<1024;j++){
-                    for(k=0;k<8;k++){
-                        temp[i]->b[j].kv[k].key="NULL";
-                    }
-                }
-            }else{
-                temp[i]=myhash->directory[i/2];
-            }
-            */
+    for(i=0;i<newsize;i++){
            temp[i]=myhash->directory[i/2];
         } //更新新目录的各个指向 
         struct segment *p=myhash->directory[loc1];
@@ -170,77 +150,112 @@ int main(int argc,char **argv){
     insert(myhash,"wadaasd",1111);
     insert(myhash,"wadaasd",1111);
     insert(myhash,"wadaasd",1111); //8个 
-    
-    if(insert(myhash,"wadaasd",1111)==false){
-        expand(myhash,"wadaasd");
-    }  
     /*
     if(insert(myhash,"wadaasd",1111)==false){
-    string key="wadaasd";
-    std::hash<string> trans;
-    size_t hash_value =trans(key);
-    int i,j,k;
-    int loc1=hash_value>>(64-myhash->global_depth);
-    myhash->global_depth=myhash->global_depth+1;
-    int newsize=1<<myhash->global_depth;
-    newtemp=new struct segment*[newsize];
-    int loc2=loc1<<1,loc3=loc2+1;
-    for(i=0;i<newsize;i++){
-        if(i==loc2){
-            newtemp[i]=new struct segment;
-            newtemp[i]->local_depth=myhash->directory[loc1]->local_depth+1;
-            for(j=0;j<1024;j++){
-                for(k=0;k<8;k++){
-                    newtemp[i]->b[j].kv[k].key="NULL";
-                }
-            }
+        expand(myhash,"wadaasd");
+    }  */
+    if(insert(myhash,"wadaasd",1111)==false){
+       std::hash<string> trans;
+       size_t hash_value =trans("wadaasd");
+       if(myhash->global_depth==myhash->directory[hash_value>>(64-myhash->global_depth)]->local_depth){
+            string key="wadaasd";
+            std::hash<string> trans;
+            size_t hash_value =trans(key);
+            int i,j,k;
+            int loc1=hash_value>>(64-myhash->global_depth);
+            myhash->global_depth=myhash->global_depth+1;
+            int newsize=1<<myhash->global_depth;
+            newtemp=new struct segment*[newsize];
+            int loc2=loc1<<1,loc3=loc2+1;
+            for(i=0;i<newsize;i++){
+                if(i==loc2){
+                    newtemp[i]=new struct segment;
+                    newtemp[i]->local_depth=myhash->directory[loc1]->local_depth+1;
+                    for(j=0;j<1024;j++){
+                        for(k=0;k<8;k++){
+                            newtemp[i]->b[j].kv[k].key="NULL";
+                        }
+                    }
+                        
+                    }else if(i==loc3){
+                        newtemp[i]=new struct segment;
+                        newtemp[i]->local_depth=myhash->directory[loc1]->local_depth+1;
+                        for(j=0;j<1024;j++){
+                            for(k=0;k<8;k++){
+                                newtemp[i]->b[j].kv[k].key="NULL";
+                            }
+                        }
+                    }else{
+                        newtemp[i]=myhash->directory[i/2];
+                    }
+                    
+                } //更新新目录的各个指向 
                 
-            }else if(i==loc3){
-                newtemp[i]=new struct segment;
-                newtemp[i]->local_depth=myhash->directory[loc1]->local_depth+1;
-                for(j=0;j<1024;j++){
-                    for(k=0;k<8;k++){
-                        newtemp[i]->b[j].kv[k].key="NULL";
+                for(i=0;i<1024;i++){
+                for(j=0;j<8;j++){
+                    if(myhash->directory[loc1]->b[i].kv[j].key!="NULL"){
+                            std::hash<string> temptrans;
+                            size_t temp_hash_value =temptrans(myhash->directory[loc1]->b[i].kv[j].key);
+                            if(temp_hash_value>>(64-myhash->global_depth)==loc2){
+                                for(k=0;k<8;k++){
+                                if(newtemp[loc2]->b[hash_value&1023].kv[k].key=="NULL"){
+                                        newtemp[loc2]->b[hash_value&1023].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
+                                        newtemp[loc2]->b[hash_value&1023].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
+                                        break;
+                                        }
+                                                }
+                            }else{
+                                for(k=0;k<8;k++){
+                                if(newtemp[loc3]->b[hash_value&1023].kv[k].key=="NULL"){
+                                        newtemp[loc3]->b[hash_value&1023].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
+                                        newtemp[loc3]->b[hash_value&1023].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
+                                        break;
+                                        }
+                            }
                     }
                 }
-            }else{
-                newtemp[i]=myhash->directory[i/2];
+                }
+                
+            }
+            struct segment *p=myhash->directory[loc1];
+            myhash->directory=newtemp;
+            
+            delete p;
+            p=NULL;
+
+               //目录和段深度一致，需要目录倍增
+       }else{
+            single_segment = new struct segment;
+            int i,j,k;
+            int loc1=hash_value>>(64-myhash->global_depth),loc2;
+            myhash->directory[loc1]->local_depth++;
+            for(i=0;i<1024;i++){
+                for(j=0;j<8;j++){
+                    if(myhash->directory[loc1]->b[i].kv[j].key!="NULL"){
+                            std::hash<string> temptrans;
+                            size_t temp_hash_value =temptrans(myhash->directory[loc1]->b[i].kv[j].key);
+                            if(temp_hash_value>>(64-myhash->global_depth)==loc2){
+                                for(k=0;k<8;k++){
+                                if(single_segment->b[hash_value&1023].kv[k].key=="NULL"){
+                                        single_segment->b[hash_value&1023].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
+                                        single_segment->b[hash_value&1023].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
+                                        break;
+                                        }
+                                                }
+                            }else{
+                                ;
+                    }
+                }
+                }
+                
             }
             
-        } //更新新目录的各个指向 
-        
-        for(i=0;i<1024;i++){
-           for(j=0;j<8;j++){
-              if(myhash->directory[loc1]->b[i].kv[j].key!="NULL"){
-                    std::hash<string> temptrans;
-                    size_t temp_hash_value =temptrans(myhash->directory[loc1]->b[i].kv[j].key);
-                    if(temp_hash_value>>(64-myhash->global_depth)==loc2){
-                        for(k=0;k<8;k++){
-                           if(newtemp[loc2]->b[hash_value&1023].kv[k].key=="NULL"){
-                                  newtemp[loc2]->b[hash_value&1023].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
-                                  newtemp[loc2]->b[hash_value&1023].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
-                                  break;
-                                  }
-                                        }
-                    }else{
-                        for(k=0;k<8;k++){
-                           if(newtemp[loc3]->b[hash_value&1023].kv[k].key=="NULL"){
-                                  newtemp[loc3]->b[hash_value&1023].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
-                                  newtemp[loc3]->b[hash_value&1023].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
-                                  break;
-                                  }
-                    }
-              }
-           }
-        }
-        
+            
+       }         //目录和段深度不一致，仅仅需要扩展段即可
+
+
     }
-    struct segment *p=myhash->directory[loc1];
-    myhash->directory=newtemp;
-    
-    delete p;
-    p=NULL;
-    }*/
+
     
     int value1;
     search(myhash,"wadaasd",value1);
