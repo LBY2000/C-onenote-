305aae05d1554e4f495e10d2ca3ab2bc560b9e85
lby2000 <2504054393@qq.com>
2023-03-16 19:27:53 +0800

    Extendible_hash

--- 目前成功的单线程可扩展hash/newTest(16B64B).cpp
+++ 目前成功的单线程可扩展hash/newTest(16B64B).cpp
@@ -7,12 +7,14 @@
 
 using namespace std;
 using namespace chrono;
-#define N 203000
-#define INITIAL 4096
-#define DEPTH 12
-#define BUC 1024
-#define MOD 1023
+#define N 80000000   //读写操作规模
+#define INITIAL 2048 //初始可扩展hash的段个数
+#define DEPTH 11     //初始深度，包括全局和局部
+#define BUC 1024     //段内桶数
+#define MOD 1023     //桶内槽数
+#define LINER 100    //线性探测距离
 
+int global_number=0;
 
 struct KV{
     size_t key;
@@ -38,20 +40,19 @@ bool insert(struct ex_hash *myhash,size_t key,size_t value){
     int loc1=hash_value>>(64-myhash->global_depth);
     int loc2=hash_value&MOD;
     int i,j,k; 
-    for(i=loc2;i<(loc2+5)&&i<BUC;i++){    //线性探测
+    for(i=loc2;i<(loc2+LINER);i++){    //线性探测
         for(j=0;j<4;j++){
-           if(myhash->directory[loc1]->b[i].kv[j].key==0){
+           if(myhash->directory[loc1]->b[i%BUC].kv[j].key==0){
                empty=true;
-               myhash->directory[loc1]->b[i].kv[j].key=key;
-               myhash->directory[loc1]->b[i].kv[j].value=value;
-               myhash->directory[loc1]->b[i].kv[j].key=key;
-               myhash->directory[loc1]->b[i].kv[j].value=value;
+               myhash->directory[loc1]->b[i%BUC].kv[j].key=key;
+               myhash->directory[loc1]->b[i%BUC].kv[j].value=value;
+
+               j=5;
+               i=2000;
                break;
            }
         }
-        if(empty){
-            break;
-        }
+
     }
     return empty;
 }
@@ -59,18 +60,23 @@ bool search(ex_hash *myhash,size_t key,size_t &value){
     bool find=false;
     std::hash<string> trans;
     size_t hash_value =trans(std::to_string(key));
+    
     int loc1=hash_value>>(64-myhash->global_depth);
     int loc2=hash_value&MOD;
     int i,j;
-    for(i=loc2;i<(loc2+5)&&i<BUC;i++){
+    for(i=loc2;i<(loc2+LINER);i++){
         for(j=0;j<4;j++){
-            if(myhash->directory[loc1]->b[i].kv[j].key==key){
+            if(myhash->directory[loc1]->b[i%BUC].kv[j].key==key){
                 find=true;
-                value=myhash->directory[loc1]->b[i].kv[j].value;
-                value=myhash->directory[loc1]->b[i].kv[j].value;
+                value=myhash->directory[loc1]->b[i%BUC].kv[j].value;
+
+                j=5;
+                i=2000;
+                break;
             }
         }
     }
+    return find;
 }
 
 
@@ -95,17 +101,12 @@ int main(int argc,char **argv){
     auto t4 = Clock::now();//计时开始
     myhash->global_depth=DEPTH;
     myhash->directory=temp;
-    insert(myhash,1111321,123);
-    insert(myhash,1111432,1234);
+
     
     size_t value,value1;
-    if(search(myhash,1111432,value)){
-        cout<<"value of 1111432(1234) is:"<<value<<endl;
-    }
-    size_t ss[N];
-    std::hash<string> trans22;
-    size_t hash_value2222 =trans22(std::to_string(12345));
-    cout<<"hash_value"<<hash_value2222<<endl;
+
+    size_t *ss=new size_t[N];
+
     auto t1 = Clock::now();//计时开始
     for(size_t time=0;time<N;time++){
        size_t rs=(time*2+1);
@@ -143,19 +144,21 @@ int main(int argc,char **argv){
                          temp[i]=myhash->directory[i/2];
                      }  //先将旧目录倍增以及增加新段
                 }
-                
+                bool inner_empty;
                 for(i=0;i<BUC;i++){
                     for(j=0;j<4;j++){
                         if(myhash->directory[loc1]->b[i].kv[j].key!=0){
+
                             inner_hash_value=trans2(std::to_string(myhash->directory[loc1]->b[i].kv[j].key));
                             if(inner_hash_value>>(64-myhash->global_depth)==loc2){
-                                  for(t=inner_hash_value&BUC;(t<inner_hash_value&BUC+5)&&t<BUC;t++){
-                                    bool inner_empty=false;
+                                  for(t=inner_hash_value&MOD;((t<inner_hash_value&MOD)+LINER);t++){
+                                    inner_empty=false;
                                     for(k=0;k<4;k++){
-                                        if(temp[loc2]->b[t].kv[k].key==0){
-                                            temp[loc2]->b[t].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
-                                            temp[loc2]->b[t].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
+                                        if(temp[loc2]->b[t%BUC].kv[k].key==0){
+                                            temp[loc2]->b[t%BUC].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
+                                            temp[loc2]->b[t%BUC].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
                                             inner_empty=true;
+                                            global_number++;
                                             break;
                                         }
                                     }
@@ -164,13 +167,14 @@ int main(int argc,char **argv){
                                     }
                                   }
                             }else{
-                                  for(t=inner_hash_value&BUC;(t<inner_hash_value&BUC+5)&&t<BUC;t++){
+                                  for(t=inner_hash_value&MOD;(t<(inner_hash_value&MOD)+LINER);t++){
                                     bool inner_empty=false;
                                     for(k=0;k<4;k++){
-                                        if(temp[loc3]->b[t].kv[k].key==0){
-                                            temp[loc3]->b[t].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
-                                            temp[loc3]->b[t].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
+                                        if(temp[loc3]->b[t%BUC].kv[k].key==0){
+                                            temp[loc3]->b[t%BUC].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
+                                            temp[loc3]->b[t%BUC].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
                                             inner_empty=true;
+                                            global_number++;
                                             break;
                                         }
                                     }
@@ -195,6 +199,7 @@ int main(int argc,char **argv){
             }else{
                 //这里表示是段分裂的情况
                  single_segment=new struct segment;
+                 int temp_loc1,temp_loc2,temp_loc3;
                  for(i=0;i<BUC;i++){
                     for(j=0;j<4;j++){
                         single_segment->b[i].kv[j].key=0;
@@ -202,23 +207,27 @@ int main(int argc,char **argv){
                  }
                  int loc1=hash_value>>(64-myhash->directory[hash_value>>(64-myhash->global_depth)]->local_depth);
                  loc1=loc1<<(myhash->global_depth-myhash->directory[hash_value>>(64-myhash->global_depth)]->local_depth);
-                 int loc2=loc1+(1<<(myhash->global_depth-myhash->directory[loc1]->local_depth));
                  myhash->directory[loc1]->local_depth++;
+                 int loc2=loc1+(1<<(myhash->global_depth-myhash->directory[loc1]->local_depth));
+                 std::hash<string> temptrans;
+                 size_t temp_hash_value;
                  single_segment->local_depth=myhash->directory[loc1]->local_depth;
                  for(i=0;i<BUC;i++){
                     for(j=0;j<4;j++){
                         if(myhash->directory[loc1]->b[i].kv[j].key!=0){
-                            std::hash<string> temptrans;
-                            size_t temp_hash_value =temptrans(std::to_string(myhash->directory[loc1]->b[i].kv[j].key));
+
+                            temp_hash_value =temptrans(std::to_string(myhash->directory[loc1]->b[i].kv[j].key));
                             if((temp_hash_value>>(64-myhash->directory[loc1]->local_depth))>=loc2){
-                                  for(t=temp_hash_value&BUC;t<(temp_hash_value&BUC+5)&&t<BUC;t++){
+                                  for(t=temp_hash_value&MOD;t<((temp_hash_value&MOD)+LINER);t++){
                                     bool empty=false;
                                     for(k=0;k<4;k++){
-                                        if (single_segment->b[t].kv[k].key!=0)
+
+                                        if (single_segment->b[t%BUC].kv[k].key==0)
                                         {
-                                            single_segment->b[t].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
-                                            single_segment->b[t].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
+                                            single_segment->b[t%BUC].kv[k].key=myhash->directory[loc1]->b[i].kv[j].key;
+                                            single_segment->b[t%BUC].kv[k].value=myhash->directory[loc1]->b[i].kv[j].value;
                                             myhash->directory[loc1]->b[i].kv[j].key=0;
+                                            global_number++;
                                             empty=true;
                                             break;
                                         }
@@ -229,16 +238,16 @@ int main(int argc,char **argv){
                                     }
                                   }
 
-                            }else{
-
                             }
-                        }
+
+                        } //有一个有效的键值对
                     }
                  }
             int direct=2*loc2-loc1;
             for(i=loc2;i<direct;i++){
                 myhash->directory[i]=single_segment;
             }
+
             }
 
 
@@ -249,63 +258,61 @@ int main(int argc,char **argv){
     auto t2 = Clock::now();//计时结束
     cout<<"global_depth is:"<<myhash->global_depth<<endl;
      bool R=true;
-    
-    for(int z=0;z<N-1;z++){
+    int number=0;
+    for(int z=0;z<N;z++){
         
         search(myhash,ss[z],value);
-        search(myhash,ss[z+1],value1);
-        if(value==value1){
+        if(z==6788401){
+        //    cout<<"z="<<z<< " and value="<<value<<endl;
+        }
+        if(value!=(z+1)){
+   //         cout<<z<<"  "<<value<<endl;
+            number++;
             R=false;
-            cout<<"wrong"<<endl;         
-            break;
-        }else if(value!=(z+1)&&value1!=(z+2)){
-            cout<<"wrong"<<endl;
-            R=false;
-            break;
+            continue;
         }
         
+    }
+    if(R){
+        cout<<"output right."<<endl;
+    }else{
+        cout<<"wrong number:"<<number<<endl;
+        cout<<"迁移的数据量:"<<global_number<<endl;
     }
     bool find=false;
     std::hash<string> trans2;
     size_t hash_value2;
     int loc11;
     int loc21;
-   // size_t hash_table[N];
- /*
-    for(int p=0;p<N;p++){
-        hash_table[p]=trans2(ss[p]);
-    }*/
+
     auto t5 = Clock::now();//计时开始
     float m=100,n=124,x=234,y=321;
     for(int z=0;z<N;z++){
-  //  auto t11 = Clock::now();
        hash_value2 =trans2(std::to_string(ss[z]));
        loc11=hash_value2>>(64-myhash->global_depth);
        loc21=hash_value2&MOD;
-       for(i=loc21&MOD;i<(loc21+5)&&i<BUC;i++){
+       for(i=loc21;i<(loc21+LINER);i++){
           for(j=0;j<4;j++){
-             if(myhash->directory[loc11]->b[i].kv[j].key==ss[z]){
+             if(myhash->directory[loc11]->b[i%BUC].kv[j].key==ss[z]){
+                  j=5;
+                  i=2000;
                   find=true;
-                  value=myhash->directory[loc11]->b[i].kv[j].value;
+                  value=myhash->directory[loc11]->b[i%BUC].kv[j].value;
              }
+
         }
+
     }
      find=false;
-  //   auto t12 = Clock::now();
-   //  std::chrono::duration_cast<std::chrono::nanoseconds>(t12 - t11).count();
-   //  std::chrono::duration_cast<std::chrono::nanoseconds>(t12 - t11).count();
-  //   std::chrono::duration_cast<std::chrono::nanoseconds>(t12 - t11).count();
- //    std::chrono::duration_cast<std::chrono::nanoseconds>(t12 - t11).count();
-//     std::chrono::duration_cast<std::chrono::nanoseconds>(t12 - t11).count();
     }
 
     auto t6 = Clock::now();//计时开始
-    if(R){
-        cout<<"right"<<endl;
-    }
-    std::cout<<"203000次操作延迟:" <<std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()<< '\n';
-    std::cout<<"初始化延迟:" <<std::chrono::duration_cast<std::chrono::nanoseconds>(t4 - t3).count()<< '\n';
-    std::cout<<"读取延迟:" <<std::chrono::duration_cast<std::chrono::nanoseconds>(t6 - t5).count()<< '\n';
 
+    std::cout<<N<<"次操作延迟:" <<float(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count())/1000000<<"ms"<<'\n';
+    std::cout<<"初始化延迟:" <<float(std::chrono::duration_cast<std::chrono::nanoseconds>(t4 - t3).count())/1000000<<"ms"<< '\n';
+    std::cout<<N<<"次读取延迟:" <<float(std::chrono::duration_cast<std::chrono::nanoseconds>(t6 - t5).count())/1000000<<"ms"<<'\n';
+    float space=(float)N,total=1<<(2+10+myhash->global_depth),ratio=space/total;
+    cout<<"空间利用率:"<<ratio<<endl;
+    
     return 0;
 }
